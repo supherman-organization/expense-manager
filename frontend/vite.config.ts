bc0508825031401b43b436195cd18ed2230260a4
Elloh-Chiaye <156177975+Elloh-Chiaye@users.noreply.github.com>
2026-08-14 15:58:05 +0200

    fix(resolve-conflict)::update configurations files

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -2,14 +2,7 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import tailwindcss from '@tailwindcss/vite';
 
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [react(), tailwindcss()],
-});
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [react(), tailwindcss()],  
+});
